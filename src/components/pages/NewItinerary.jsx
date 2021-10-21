a24f5250e6e280297cc232fcec8668aee1a87985
Takapy1 <takahirog1109mai@gmail.com>
2021-10-21 18:02:22 +0900

    [Fix] NewItinerary page

--- src/components/pages/NewItinerary.jsx
+++ src/components/pages/NewItinerary.jsx
@@ -10,10 +10,17 @@ const NewItinerary = () => {
 
   }
 
+  // 場所追加ボタンにより発火（動的にPlanのフォームを呼び出す）
+  // まだ決定ボタンが押されていなかった時にどうするか要相談
+  const addPlan = () => {
+    
+  }
+
   return (
     <>
       <h2>しおり新規作成画面だよ</h2>
       <FormPlan />
+      <button onClick={addPlan}>場所追加</button>
       <button onClick={createItinerary}>しおりを保存</button>
     </>
   );
